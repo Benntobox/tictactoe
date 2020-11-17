3838b89118dd1bb55835ebed7e4c35b157be0eda
Benntobox <benjaminsboyle3@gmail.com>
2020-11-17 14:47:32 -0800

    Working buttons, all html elements added in app.js

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var addButton = function () {
+let rows = ['top', 'middle', 'bottom'];
+let columns = ['left', 'center', 'right']
+let curr = 0;
+
+var makeButton = function (...classnames) {
   let button = document.createElement('button');
   button.innerHTML = 'Empty';
-  document.appendChild(button);
+  button.onclick = updateButton.bind(button);
+  classnames.map(classname => button.classList.add(classname));
+  return button;
 }
 
-var updateButton = function(button) {
-  button.innerHTML = "X";
+var updateButton = function() {
+  console.log('pressed');
+  this.innerHTML = "X";
 }
+
+var addAllButtons = function () {
+  for (let row of rows) {
+    let newRow = document.createElement('div');
+    newRow.classList.add(row);
+    for (let column of columns) {
+      let button = makeButton(row, column);
+      newRow.appendChild(button);
+    }
+    document.body.appendChild(newRow);
+  }
+}
+
+addAllButtons();
